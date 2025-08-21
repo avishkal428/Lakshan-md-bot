319db16c152ef661d29ca0c7aa485ccefa49e69b
avishkal428 <avishkal907@gmail.com>
2025-08-21 18:46:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "qQtVnbhL#SxtU8CtLLh8AFEFWDt9ySwsBW92IpfBFVIEIB5tOfXA",
     ALIVE_IMG: process.env. ALIVE_IMG || "https://photos.app.goo.gl/4jgu6cW35kSLQLg5A",
     ALIVE_MSG:  process.env. ALIVE_MSG || "Hellow,I am YourName i am alive now!",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
